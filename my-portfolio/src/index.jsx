682ed10425a7d7334ef1a78870a7801ef167ff36
MOHAED-ABDALFATTAH <mohamed.abozeid@ejust.edu.eg>
2025-09-25 22:21:35 +0300

    v5

--- my-portfolio/src/index.jsx
+++ my-portfolio/src/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Portfolio from "./Portfolio";
-import "./index.css"; // TailwindCSS styles
+import "./index.css"; // global CSS
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
