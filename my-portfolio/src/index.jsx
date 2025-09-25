3b206143313e59977698437dc0a2c25d1a0a818f
MOHAED-ABDALFATTAH <mohamed.abozeid@ejust.edu.eg>
2025-09-25 22:27:23 +0300

    v7

--- my-portfolio/src/index.jsx
+++ my-portfolio/src/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Portfolio from "./Portfolio";
+import Portfolio from "./App";
 import "./index.css"; // global CSS
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
